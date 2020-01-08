a67776d5cea24e10c6482be46b6acc3f339d8e02
pengyh <pt370390794@126.com>
2020-01-08 23:30:00 +0800

    提交

--- src/item.js
+++ src/item.js
@@ -1,20 +1,27 @@
 import React, { Component} from 'react';
-
+import Proptypes from 'prop-types'
 class Item extends Component {
 
     render() {
         return (
             <li>
-                <span>{this.props.content}</span>
+                <span>{this.props.name}给你做{this.props.content}</span>
                 <button onClick={this.handleDel}>删除</button>
             </li>
         )
     }
 
     handleDel = ()=>{
-        console.log(this.props.index)
         this.props.delItem(this.props.index)
     }
 }
-
+Item.propTypes={
+    index:Proptypes.number,
+    content:Proptypes.string,
+    delItem:Proptypes.func,
+    name:Proptypes.string.isRequired
+}
+Item.defaultProps={
+    name:'王小丫'
+}
 export default Item
--- src/list.js
+++ src/list.js
@@ -8,12 +8,24 @@ class App extends Component {
             list: ['服务一', '服务二'],
         }
     }
+    UNSAFE_componentWillMount(){
+        console.log('将要挂载')
+    }
+    componentDidMount(){
+        console.log('挂载好了')
+    }
     render() {
+        console.log('挂载中')
         return (
             <Fragment>
-                <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)}></input>
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleInput}
+                    ref={(e) => this.input = e}
+                ></input>
                 <button onClick={this.addList}>添加服务</button>
-                <ul className='pyh'>
+                <ul className='pyh' ref={(e) => this.ul = e}>
                     {
                         this.state.list.map((item, index) => {
                             return (
@@ -30,17 +42,19 @@ class App extends Component {
             </Fragment>
         )
     }
-    handleInput(e) {
+    handleInput = () => {
         this.setState({
-            value: e.target.value
+            value: this.input.value
         })
     }
     addList = () => {
         this.setState({
             list: [...this.state.list, this.state.value]
+        },() =>{
+            console.log(this.ul.querySelectorAll('li').length)
         })
     }
-    delItem =(index)=> {
+    delItem = (index) => {
         let list = this.state.list;
         list.splice(index, 1);
         this.setState({
